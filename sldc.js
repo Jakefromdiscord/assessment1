28ebea2111bcf918e23f74b32bdb7c6dbfc1e45f
Jacob <jacobwamsley12@gmail.com>
2022-01-07 08:48:43 -0700

    SLDC section complete

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// 1. Planning
+// 2. Analysis of Requirements
+// 3. Design
+// 4. Implementation
+// 5. Testing & Integration
+// 6. Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "After planning teams will then create a docuement typically called the 'requirement specification document'. This will outline to the team what was determined in the planning stage to get further feedback and ask questions. Also helps the team understand the timeline in which they are working. Helps teams also know potential risks involved with the project."
+const design = "The information from the above to steps are gone over by the team to create some sort 'architecture' of the software being developed."
+const Implementation = "In this stage the developement team has all the requirements for their project and the design of the software. They being creating the source code for the software."
+const testingAndIntegration = "In this phase of the life cycle parts of the code will be given out to developers to rigorously test for any bugs. The developers will test these modules according the the needs and architecture of the software they are developing. Once testing is complete the software will then be deployed for user testing and acceptance or straight into production."
+const Maintain = "At this point the software may have some issues once being used and a team will be there to make sure it works the way it was intended and help maintain that. They may also add additional features to the software as time goes on."
